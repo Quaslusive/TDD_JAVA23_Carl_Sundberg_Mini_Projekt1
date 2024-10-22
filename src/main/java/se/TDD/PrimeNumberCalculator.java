ab96b5619ae43695b0bee266ea58195035b06233
Pawlo Picasso <Quaslusive@protonmail.com>
2024-10-23 00:27:59 +0200

    Initial commit

---
+++ src/main/java/se/TDD/PrimeNumberCalculator.java
@@ -0,0 +1,4 @@
+package se.TDD;
+
+public class PrimeNumberCalculator {
+}
---
+++ src/test/java/se/TDD/PrimeNumberCalculatorTest.java
@@ -0,0 +1,7 @@
+package se.TDD;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeNumberCalculatorTest {
+
+}
